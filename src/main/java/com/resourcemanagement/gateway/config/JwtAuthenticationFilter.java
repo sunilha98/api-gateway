adb01250cae555c96013028353610dc4220c9b7f
Sunil Hawale <Sunil.Laxmanhawale@dexian.com>
2025-08-11 19:54:27 +0530

    Feature: Checked blacklisted tokens

--- src/main/java/com/resourcemanagement/gateway/config/JwtAuthenticationFilter.java
+++ src/main/java/com/resourcemanagement/gateway/config/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.resourcemanagement.gateway.config;
 
+import com.resourcemanagement.gateway.blacklisteds.service.BlacklistedTokenService;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
@@ -15,10 +16,12 @@ import reactor.core.publisher.Mono;
 public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAuthenticationFilter.Config> {
 
     private final JwtTokenUtil jwtTokenUtil;
+    private final BlacklistedTokenService blacklistedTokenService;
 
-    public JwtAuthenticationFilter(JwtTokenUtil jwtTokenUtil) {
+    public JwtAuthenticationFilter(JwtTokenUtil jwtTokenUtil, BlacklistedTokenService blacklistedTokenService) {
         super(Config.class);
         this.jwtTokenUtil = jwtTokenUtil;
+        this.blacklistedTokenService = blacklistedTokenService;
     }
 
     @Override
@@ -34,6 +37,10 @@ public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAut
             String authHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
             String token = authHeader.replace("Bearer ", "");
 
+            if (blacklistedTokenService.isBlacklisted(token)) {
+                return this.onError(response, HttpStatus.UNAUTHORIZED, "Token has been blacklisted");
+            }
+
             if (!jwtTokenUtil.validateToken(token)) {
                 return this.onError(response, HttpStatus.UNAUTHORIZED, "Invalid or expired token");
             }
@@ -41,10 +48,11 @@ public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAut
             Claims claims = jwtTokenUtil.getAllClaimsFromToken(token);
             String username = claims.getSubject();
             String email = (String) claims.get("email");
-            String role = (String) claims.get("role").toString();
+            String role = claims.get("role").toString();
 
             ServerHttpRequest modifiedRequest = request.mutate().header("X-Auth-Username", username)
-                    .header("X-Auth-Email", email).header("X-Auth-Role", role).build();
+                    .header("X-Auth-Email", email).header("X-Auth-Role", role)
+                    .header("X-Bearer-Token", token).build();
 
             return chain.filter(exchange.mutate().request(modifiedRequest).build());
         };
